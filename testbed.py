00442b3097bf103bc9cdcfb1006ff3915792f608
socce <socce@LAPTOP-8H65IUEJ.local>
2021-04-08 21:52:55 -0700

    Update testbed.py

--- testbed.py
+++ testbed.py
@@ -20,6 +20,8 @@ class Testbed():
     def __init__(self):
         self.cone_pul = 
         self.cone_dir = 
+        self.reel_pul =
+        self.reel_dir = 
 
         self.table_pwm = 
         self.table_in1 = 
@@ -27,8 +29,26 @@ class Testbed():
         self.table_en =
 
         self.limit_pin = 
-        GPIO.setup(self.limit_pin, GPIO.INe)
+        GPIO.setup(self.limit_pin, GPIO.IN)
         self.contact_pin = 
+        GPIO.setup(self.contact_pin, GPIO.IN)
         self.table_hall_pin = 
+        GPIO.setup(self.table_hall_pin, GPIO.IN)
+
+    def main(self):
+        pass
+
+    def raise_cone(self):
+        pass
+
+    def lower_cone(self):
+        pass
+
+    def reel_in_spool(self):
+        pass
+
+    def reel_out_spool(self):
+        pass
+
 
 
